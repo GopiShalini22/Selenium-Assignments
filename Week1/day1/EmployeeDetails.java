331608a8378369f87faee8bf834d39cfa97a8999
GopiShalini22 <krishnanggopi@gmail.com>
2025-09-13 13:37:27 +0530

    Add files via upload

---
+++ Week1/day1/EmployeeDetails.java
@@ -0,0 +1,28 @@
+package Week1.day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String name = "John Doe";
+		int age = 30;
+		double salary = 55000.75;
+		char gender = 'M';
+		boolean acceptTerms = true;
+		float rating = 4.5f;
+		long mobileNumber = 9876543210l;
+		int pinCode = 56001;
+		byte luckyNumber = 7;
+		System.out.println("Name: "+name+"\n"+
+		"Age    : "+age+"\n"+
+		"Salary : "+salary+"\n"+
+		"Gender : "+gender+"\n"+
+		"AcceptTerms: "+acceptTerms+"\n"+
+		"Rating : "+rating+"\n"+
+        "MobileNumber : "+mobileNumber+"\n"+
+		"PinCode : "+pinCode+"\n"+
+        "LuckyNumber : "+luckyNumber+"\n");
+	}
+
+}
---
+++ Week1/day1/Firefox.java
@@ -0,0 +1,15 @@
+package Week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float browserVersion = 100.2f;
+		String browserName = "firefox"; 
+        Boolean isVisible = true; 
+        int releaseYear = 1998;
+        char browserLogo = 'f';
+        System.out.println("float:"+browserVersion+"\n"+"string:"+browserName+"\n"+"boolean:"+isVisible+"\n"+"int:"+releaseYear+"\n"+"char:"+browserLogo);
+	}
+
+}
